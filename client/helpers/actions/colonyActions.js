0c5d023a49db26dc231b915983c39ca14e576c8f
Ryan Christoffersen <ryan@ryanchristo.com>
2018-06-19 19:58:13 +0700

    create domains on create colony

--- client/helpers/actions/colonyActions.js
+++ client/helpers/actions/colonyActions.js
@@ -2,8 +2,25 @@
 
 export const createColony = async (networkClient, tokenAddress) => {
 
+  // create colony
   const { eventData: { colonyAddress } } = await networkClient.createColony.send({ tokenAddress })
 
+  // get colony client
+  const colonyClient = await networkClient.getColonyClientByAddress(colonyAddress)
+
+  // get local skill id from root domain
+  const { localSkillId } = await colonyClient.getDomain.call({ domainId: 1 })
+
+  // add business domain
+  const businessDomain = await colonyClient.addDomain.send({ parentSkillId: localSkillId })
+
+  // add education domain
+  const educationDomain = await colonyClient.addDomain.send({ parentSkillId: localSkillId })
+
+  // add service domain
+  const serviceDomain = await colonyClient.addDomain.send({ parentSkillId: localSkillId })
+
+  // return colony address
   return colonyAddress
 
 }
