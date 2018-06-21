37208858804b4c9686b5e44449d7eb08b6d5ccd3
Ryan Christoffersen <ryan@ryanchristo.com>
2018-06-22 02:01:13 +0700

    added test admins on create

--- helpers/actions/colonyActions.js
+++ helpers/actions/colonyActions.js
@@ -14,6 +14,12 @@ export const createColony = async (networkClient, tokenAddress) => {
   // set current user as user role admin
   await colonyClient.authority.setUserRole.send({ user: colonyClient.adapter.wallet.address, role: 'ADMIN' })
 
+  // set test user as user role admin
+  await colonyClient.authority.setUserRole.send({ user: '0xffcf8fdee72ac11b5c542428b35eef5769c409f0', role: 'ADMIN' })
+
+  // set test user as user role admin
+  await colonyClient.authority.setUserRole.send({ user: '0x22d491bde2303f2f43325b2108d26f1eaba1e32b', role: 'ADMIN' })
+
   // get local skill id from root domain
   const { localSkillId } = await colonyClient.getDomain.call({ domainId: 1 })
 
